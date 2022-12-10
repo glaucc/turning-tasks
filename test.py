df5af8af6d2dbc0cfd961eac48ac4148a9e94cac
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-12-10 12:38:17 +0400

    failed

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
 #test
+def findErrorNums(nums):
+	numbers = []
+	for x in nums:
+		numbers.append(x)
+	result = []
+	output = []
+	#1,2,3,4
+    val = len(numbers)
+    val = val + 1
+	for i in range(val):
+		result.append(i)
+	for j in numbers:
+		for n in result:
+			if j != n:
+				output.append(j)
+				output.append(n)
+	return output
+
+if __name__ == '__main__':
+	line = input()
+	components = line.strip().split()
+	components = [int(component) for component in components]
+
+	print(findErrorNums(components))
